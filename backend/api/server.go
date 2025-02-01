f5b702a9245f79ef313ba000470676f8e19bf533
Wiblz <wiblzcode@gmail.com>
2025-02-01 22:10:17 +0100

    Add a request logging middleware

--- backend/api/server.go
+++ backend/api/server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Wiblz/Fun-Invoice-Manager/backend/storage/filestore"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 
@@ -28,11 +29,34 @@ func (s *Server) Shutdown() error {
 	return nil
 }
 
+// responseWriter is a wrapper around http.ResponseWriter that captures the status code for logging purposes
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		ip := r.RemoteAddr
+
+		// Create a response writer to capture the status code
+		rw := &responseWriter{w, http.StatusOK}
+
+		next.ServeHTTP(rw, r)
+
+		duration := time.Since(start)
+		log.Printf("[%s] %s - %v in %v (%s)", r.Method, r.URL.Path, rw.statusCode, duration, ip)
+	})
+}
+
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Log the request for debugging
-		log.Printf("Request: Method: %s, URL: %s", r.Method, r.URL)
-
 		// Set CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
@@ -60,6 +84,7 @@ func NewServer(storageManager *db.Manager, filestoreClient *filestore.Client) *S
 	r := mux.NewRouter()
 	apiRouter := r.PathPrefix("/api/v1").Subrouter()
 	apiRouter.Use(corsMiddleware)
+	apiRouter.Use(loggingMiddleware)
 	apiRouter.HandleFunc("/invoices", s.GetAllInvoicesHandler).Methods("GET")
 	apiRouter.HandleFunc("/invoice/{hash}/review-status", s.SetReviewedStatus).Methods("PATCH")
 	apiRouter.HandleFunc("/invoice/{hash}/payment-status", s.SetPaidStatus).Methods("PATCH")
